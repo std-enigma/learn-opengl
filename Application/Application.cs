4bfd416cf3b9a78577cf2486399602bff1203a06
std-enigma <std.ilia.bahrebar@gmail.com>
2025-12-04 08:56:18 +0330

    Complete Hello-Triangle section

--- Application/Application.cs
+++ Application/Application.cs
@@ -8,6 +8,9 @@ using Silk.NET.Windowing;
 
 internal class Application
 {
+    private uint _vao;
+    private uint _program;
+
     private GL? _gl;
     private readonly IWindow _window;
 
@@ -25,7 +28,7 @@ internal class Application
     }
 
     // Load andn initialize application resources
-    private void OnLoad()
+    private unsafe void OnLoad()
     {
         var input = _window.CreateInput();
         foreach (var keyboard in input.Keyboards)
@@ -33,12 +36,95 @@ internal class Application
 
         _gl = _window.CreateOpenGL();
         _gl.ClearColor(Color.CornflowerBlue);
+
+        _vao = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vao);
+
+        var vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+
+        var vertices = new float[] { 0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f };
+        fixed (float* buf = vertices)
+            _gl.BufferData(
+                BufferTargetARB.ArrayBuffer,
+                (nuint)(vertices.Length * sizeof(float)),
+                buf,
+                BufferUsageARB.StaticDraw
+            );
+
+        var vertCode =
+            @"
+            #version 330 core
+
+            layout (location = 0) in vec3 aPos;
+
+            void main()
+            {
+                gl_Position = vec4(aPos, 1.0);
+            }
+            ";
+        var vert = _gl.CreateShader(ShaderType.VertexShader);
+        _gl.ShaderSource(vert, vertCode);
+        _gl.CompileShader(vert);
+        _gl.GetShader(vert, ShaderParameterName.CompileStatus, out int vStatus);
+        if (vStatus != (int)GLEnum.True)
+            throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vert));
+
+        var fragCode =
+            @"
+            #version 330 core
+
+            out vec4 FragColor;
+
+            void main() {
+                FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+            }
+            ";
+        var frag = _gl.CreateShader(ShaderType.FragmentShader);
+        _gl.ShaderSource(frag, fragCode);
+        _gl.CompileShader(frag);
+        _gl.GetShader(frag, ShaderParameterName.CompileStatus, out int fStatus);
+        if (fStatus != (int)GLEnum.True)
+            throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(frag));
+
+        _program = _gl.CreateProgram();
+        _gl.AttachShader(_program, vert);
+        _gl.AttachShader(_program, frag);
+        _gl.LinkProgram(_program);
+        _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
+        if (lStatus != (int)GLEnum.True)
+            throw new Exception(
+                "Failed to link the shader program: " + _gl.GetProgramInfoLog(_program)
+            );
+
+        _gl.DetachShader(_program, vert);
+        _gl.DetachShader(_program, frag);
+        _gl.DeleteShader(vert);
+        _gl.DeleteShader(frag);
+
+        const uint posLoc = 0;
+        _gl.EnableVertexAttribArray(posLoc);
+        _gl.VertexAttribPointer(
+            posLoc,
+            3,
+            VertexAttribPointerType.Float,
+            false,
+            3 * sizeof(float),
+            (void*)0
+        );
+
+        _gl.BindVertexArray(0);
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
     }
 
     // Render loop
     private void OnRender(double deltaTime)
     {
         _gl?.Clear(ClearBufferMask.ColorBufferBit);
+
+        _gl?.UseProgram(_program);
+        _gl?.BindVertexArray(_vao);
+        _gl?.DrawArrays(PrimitiveType.Triangles, 0, 3);
     }
 
     // Handle window resizing
